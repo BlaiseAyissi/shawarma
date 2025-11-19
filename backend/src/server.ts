ce9d7609c5e9449feeb4d88d0f35cb8e96190b2d
BlaiseAyissi <106978174+BlaiseAyissi@users.noreply.github.com>
2025-11-19 13:03:23 +0800

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,9 +29,9 @@ app.use(
   cors({
     origin: (origin, callback) => {
       // Allow requests with no origin (like mobile apps or curl requests)
+      console.log('the origin is '+origin)
       if (!origin) return callback(null, true);
       
-      console.log('the origin is '+origin)
       if (allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
